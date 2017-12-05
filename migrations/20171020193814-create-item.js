fce458b7c04c8ddda3a6684bf14b3fedf0b46fd7
ElenaOl <pravutiner@gmail.com>
2017-12-04 23:00:47 -0800

    first commit

---
+++ migrations/20171020193814-create-item.js
@@ -0,0 +1,36 @@
+'use strict';
+module.exports = {
+  up: (queryInterface, Sequelize) => {
+    return queryInterface.createTable('items', {
+      id: {
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+        type: Sequelize.INTEGER
+      },
+      itemName: {
+        type: Sequelize.STRING
+      },
+      amount: {
+        type: Sequelize.INTEGER
+      },
+      comment: {
+        type: Sequelize.STRING
+      },
+      listId: {
+        type: Sequelize.INTEGER
+      },
+      createdAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      },
+      updatedAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      }
+    });
+  },
+  down: (queryInterface, Sequelize) => {
+    return queryInterface.dropTable('items');
+  }
+};
---
+++ models/item.js
@@ -0,0 +1,17 @@
+'use strict';
+module.exports = (sequelize, DataTypes) => {
+  var item = sequelize.define('item', {
+    itemName: DataTypes.STRING,
+    amount: DataTypes.INTEGER,
+    comment: DataTypes.STRING,
+    listId: DataTypes.INTEGER
+  }, {
+    classMethods: {
+      associate: function(models) {
+        // associations can be defined here
+        models.item.belongsTo(models.list);
+      }
+    }
+  });
+  return item;
+};
